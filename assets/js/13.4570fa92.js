792b2a619fc1094f7608ddd01897d02323a38628
Adam Plouff <adamplouff@gmail.com>
2020-07-21 09:12:35 -0400

    deploy

---
+++ assets/js/13.4570fa92.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{374:function(t,e,s){"use strict";s.r(e);var n=s(42),l=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("Redirect",{attrs:{to:"https://battleaxe.co/help"}})],1)}),[],!1,null,null,null);e.default=l.exports}}]);
