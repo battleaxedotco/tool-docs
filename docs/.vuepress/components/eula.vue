5a56bba670f432a2390ec97b65b36e870d71dea9
Adam Plouff <adamplouff@gmail.com>
2020-12-08 23:06:11 -0500

    Cleanup

--- docs/.vuepress/components/eula.vue
+++ docs/.vuepress/components/eula.vue
@@ -4,7 +4,7 @@
             Please read this End User License Agreement (“EULA”) before you download and use {{name}}.
         </p>
         <p>
-            By purchasing a License Key and/or downloading and using the {{name}}, You agree, without reservation to be bound by the terms of this EULA. If You do not agree with the terms of this EULA, please do not purchase a License Key and/or download and use {{name}}.
+            By purchasing a License Key and/or downloading and using {{name}}, You agree, without reservation to be bound by the terms of this EULA. If You do not agree with the terms of this EULA, please do not purchase a License Key and/or download and use {{name}}.
         </p>
 
         <a :href="`/eula/${name}_EULA.pdf`" class="nav-link action-button">View EULA</a>
