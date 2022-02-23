eea8393a811534d778d8a742481abb570fb36cf0
Adam Plouff <adamplouff@gmail.com>
2022-02-23 04:43:08 -0500

    embed prop

--- docs/.vuepress/components/Subscribe.vue
+++ docs/.vuepress/components/Subscribe.vue
@@ -2,6 +2,7 @@
   <main
     class="newsletter"
     aria-labelledby="main-title"
+    :class="{embed: embed}"
     :style="{'background': background}"
   >
     <div id="mc_embed_signup">
@@ -10,31 +11,42 @@
             method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank"
             novalidate>
             <div id="mc_embed_signup_scroll" style="display: flex; flex-wrap: wrap">
-                <div class="mc-field-group">
-                    <input type="email" value="" name="EMAIL" class="email-input-primary" id="mce-EMAIL"
-                        placeholder="Email" style="margin: 0 8px 8px 0">
-                </div>
-    
-                <div class="mc-field-group input-group" style="display:none">
-                    <strong>Freebie</strong>
-                    <ul>
-                        <li><input type="checkbox" value="128" name="group[17082][128]" id="mce-group[17082]-17082-0"
-                                checked><label for="mce-group[17082]-17082-0">Buttcapper</label></li>
-                        <li><input type="checkbox" value="256" name="group[17082][256]" id="mce-group[17082]-17082-1"><label
-                                for="mce-group[17082]-17082-1">Hylian</label></li>
-                        <li><input type="checkbox" value="512" name="group[17082][512]" id="mce-group[17082]-17082-2"><label
-                                for="mce-group[17082]-17082-2">Beatgrid</label></li>
-                    </ul>
-                </div>
-                <div id="mce-responses" class="clear">
-                    <div class="response" id="mce-error-response" style="display:none"></div>
-                    <div class="response" id="mce-success-response" style="display:none"></div>
-                </div>
-                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                <!-- <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
-                        name="b_4cc8eea30236019bbfce85300_0898372b84" tabindex="-1" value=""></div> -->
-                <div><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
-                        class="submit-button-primary"></div>
+              <div class="mc-field-group">
+                <input 
+                  type="email" 
+                  value="" 
+                  name="EMAIL" 
+                  class="email-input-primary" 
+                  id="mce-EMAIL"
+                  placeholder="Email" style="margin: 0 8px 8px 0">
+              </div>
+  
+              <!-- <div class="mc-field-group input-group" style="display:none">
+                  <strong>Freebie</strong>
+                  <ul>
+                      <li><input type="checkbox" value="128" name="group[17082][128]" id="mce-group[17082]-17082-0"
+                              checked><label for="mce-group[17082]-17082-0">Buttcapper</label></li>
+                      <li><input type="checkbox" value="256" name="group[17082][256]" id="mce-group[17082]-17082-1"><label
+                              for="mce-group[17082]-17082-1">Hylian</label></li>
+                      <li><input type="checkbox" value="512" name="group[17082][512]" id="mce-group[17082]-17082-2"><label
+                              for="mce-group[17082]-17082-2">Beatgrid</label></li>
+                  </ul>
+              </div> -->
+              <div id="mce-responses" class="clear">
+                  <div class="response" id="mce-error-response" style="display:none"></div>
+                  <div class="response" id="mce-success-response" style="display:none"></div>
+              </div>
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <!-- <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
+                      name="b_4cc8eea30236019bbfce85300_0898372b84" tabindex="-1" value=""></div> -->
+              <div>
+                <input 
+                  type="submit" 
+                  value="Subscribe" 
+                  name="subscribe" 
+                  id="mc-embedded-subscribe" 
+                  class="submit-button-primary">
+              </div>
             </div>
         </form>
         <div style="margin-top: 24px">{{footer}}</div>
@@ -48,17 +60,21 @@ const _ = require('lodash')
 export default {
   name: 'Newsletter',
   props: {
+    embed: {
+      type: Boolean,
+      default: false
+    },
     headline: {
       type: String,
-      default: '+++'
+      default: ''
     },
     footer: {
       type: String,
-      default: 'Footer text'
+      default: 'Download link is emailed immediately. Unsubscribe any time. We never ever share your info.'
     },
     background: {
       type: String,
-      default: '#212121'
+      default: 'transparent'
     },
   },
   data: () => ({
@@ -74,27 +90,30 @@ export default {
 
 <style scoped>
 .newsletter {
+  font-family: 'Fira Mono';
+}
+.embed {
   width: 100vw;
   height: 100vh;
   position: absolute;
   top: 0;
   left: 0;
   margin-top: 0 !important;
-  font-family: 'Fira Mono';
   display: flex;
 }
+.embed #mc_embed_signup {
+  padding: 32px 16px;
+}
 
-#mc_embed_signup {
-    /* border: 2px solid #fff; */
-    padding: 32px 16px;
-    line-height: 1.6em;
-    overflow: hidden;
-    align-self: center;
+#mc_embed_signup {  
+  line-height: 1.6em;
+  overflow: hidden;
+  align-self: center;
 }
 #mc_embed_signup h2 {
-    line-height: 1.1em;
-    margin-bottom: 24px;
-    margin-top: 0;
+  line-height: 1.1em;
+  margin-bottom: 24px;
+  margin-top: 0;
 }
 .email-input-primary {
   font-family: 'Fira Mono';
